ee48949c84cdc8136dbdc6ac342090fcea0bc6a7
Nam Tran <hoainam.tran01@gmail.com>
2022-09-25 19:10:10 +1000

    Chloropleth Map and Hotspot

--- py/country_id.py
+++ py/country_id.py
@@ -1,6 +1,6 @@
 from csv import DictReader
 
-def read_file(file_name):
+def get_country_ids(file_name):
     with open(file_name, 'r', newline='', encoding='utf-8') as file:
         reader = DictReader(file)
         countries = {row['country_id']: row['country'] for row in reader}
@@ -10,5 +10,30 @@ def read_file(file_name):
         file.writelines(f"{country_id},{country}\n" for country_id, country in countries.items())
 
 
+def aggregate_best_estimate():
+    with open('../data/country_id_mapping.csv', 'r', newline='', encoding='utf-8') as id_file, \
+         open('../data/GEDEvent_v22_1.csv', 'r', newline='', encoding='utf-8') as data_file, \
+         open('../data/best_death_estimate.csv', 'w', encoding='utf-8') as out_file:
+
+        id_reader = DictReader(id_file)
+        data_reader = DictReader(data_file)
+
+        countries = {row['country_id']: row['country_name'] for row in id_reader}
+
+        totals = {country_id: 0 for country_id in countries}
+        for row in data_reader:
+            country_id = row['country_id']
+            best = row['best']
+
+            if country_id in totals:
+                totals[country_id] += int(best)
+            else:
+                totals[country_id] = int(best)
+
+        out_file.writelines("country_id,country_name,best\n")
+
+        out_file.writelines(f"{country_id},{countries[country_id]},{totals[country_id]}\n" for country_id in countries)
+
+
 if __name__ == "__main__":
-    read_file("../data/GEDEvent_v22_1.csv")
+    aggregate_best_estimate()
